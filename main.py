7c76bf29b1e84e848b88c3dc7c6deb8160ea25be
Clendenin <clendenin@protonmail.com>
2017-10-28 16:15:24 -0500

    slight mods

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #Main file
+#!/usr/bin/python
 import praw
 
+reddit = praw.Reddit('bot1')
 
-reddit = praw.Reddit('bot1') #Creating reddit instance
+subreddit = reddit.subreddit("learnpython")
 
-subreddit = reddit.subreddit("Learnpython")
 for submission in subreddit.hot(limit=5):
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
-    print("---------\n")
+    print("---------------------------------\n")
