b40e6994d4811fcd6e06bd07cdc9e261aacc1eab
Clendenin <clendenin@protonmail.com>
2017-10-28 22:20:25 -0500

    calling reply function

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ passes to poster
 import praw
 import emojiDictionary
 import emoji
+import reply_post.py
 
 print("running...")
 reddit = praw.Reddit('bot1') #Creating reddit instance
@@ -19,16 +20,16 @@ submission.comments.replace_more(limit=0)
 for top_level_comment in submission.comments:
     emojiString = emojiDictionary.toEmojiString((top_level_comment.body))
     emojiString = emoji.emojize(emojiString, use_aliases=True)
-    if(emojiDictionary.lastHadEmoji() == True or True):
+    if(emojiDictionary.lastHadEmoji() == True):
         print(emojiString)
         print(top_level_comment)
         #Eventually pass to poster code
+        reply(emojiString, top_level_comment)
+
+def redditComments()
 
 
 
-"""
-TODO:
-Change subreddit
-Change comment selection
-
-"""
+#To do:
+#Change subreddit
+#Change comment selection
