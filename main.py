f5d7282212bade43e32d8d4a38145c3beda5cba9
Clendenin <Clendenin@protonmail.com>
2017-10-29 07:38:46 -0500

    Goes through posts and comments

--- main.py
+++ main.py
@@ -9,25 +9,33 @@ import praw
 import emojiDictionary
 import emoji
 import reply_post
+#import time
 
-reddit = praw.Reddit('bot1') #Creating reddit instance
+loop = 0
+while (loop < 1): #Eventually near infinite
 
-subreddit = input("What subreddit's comments do you want to convert(Submit URL) - type d for default subreddit: ")
-if (subreddit == "d"):
-    subreddit = reddit.subreddit('text2emoji')
-    #subreddit = ("https://www.reddit.com/r/text2emoji/")
+    reddit = praw.Reddit('bot1') #Creating reddit instance
 
-print("running...")
+    userInput = input("What subreddit's comments do you want to convert(Submit name of subreddit) - type d for default subreddit: ")
+    if (userInput == "d"):
+        subreddit = reddit.subreddit('text2emoji')
+        #subreddit = ("https://www.reddit.com/r/text2emoji/")
+    else:
+        subreddit = reddit.subreddit(userInput)
 
-#Obtains submission object
-for submission in subreddit.hot(limit=10):
-    submission.comments.replace_more(limit=0)
-    for top_level_comment in submission.comments:
-        emojiString = emojiDictionary.toEmojiString((top_level_comment.body))
-        if not emojiString == False:
-            emojiString = emoji.emojize(emojiString, use_aliases=True)
-            if(emojiDictionary.lastHadEmoji() == True):
-                print("To be posted to Reddit")
-                print(emojiString)
-                print()
-                reply_post.replyMaster(emojiString, top_level_comment)
+    print("running...")
+
+    #Obtains submission object
+    for submission in subreddit.hot(limit=10):
+        submission.comments.replace_more(limit=0)
+        for top_level_comment in submission.comments:
+            emojiString = emojiDictionary.toEmojiString((top_level_comment.body))
+            if not emojiString == False:
+                emojiString = emoji.emojize(emojiString, use_aliases=True)
+                if(emojiDictionary.lastHadEmoji() == True):
+                    print("To be posted to Reddit")
+                    print(emojiString)
+                    print()
+                    #reply_post.replyMaster(emojiString, top_level_comment)
+    loop = loop + 1 #get rid of this once the while loop is infinite
+    #time.sleep(30) #half a min.
